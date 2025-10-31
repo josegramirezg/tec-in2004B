f769a71b9af29a6a3f091d4991991b26b6ce917a
Rusbel Morales <rusbel-morales11@outlook.com>
2025-10-30 22:39:37 -0600

    Eliminación de algunos paquetes y  mejora de diseño de las cards de las materias

--- frontend/src/app/presentation/[presentationId]/PresentationPageContent.tsx
+++ frontend/src/app/presentation/[presentationId]/PresentationPageContent.tsx
@@ -2,18 +2,23 @@
 
 import { Box } from "@chakra-ui/react";
 import { PresentationPageContentProps } from "@/types/presentationPageContentProps";
+import { useState } from "react";
+import { Loading } from "@/app/(components)/Loading";
 
 export function PresentationPageContent({
   filePath,
   presentationId,
 }: PresentationPageContentProps) {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <Box h="100vh">
+      {isLoading && <Loading />}
       <iframe
         src={`/tec-in2004B/${filePath}`}
         width="100%"
         height="100%"
         title={`Presentación ${presentationId}`}
+        onLoad={() => setIsLoading(false)}
         onError={() => console.error("Error cargando presentación")}
       />
     </Box>
